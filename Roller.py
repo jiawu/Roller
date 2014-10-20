cae219a9b25635ea354d1a777d3661dde9b944da
Jia Wu <jiawu@u.northwestern.edu>
2014-10-20 18:33:07 -0500

    added a feature to only get gene values instead of other columns detailing experimental results, added a test for that as well

--- Roller.py
+++ Roller.py
@@ -1,7 +1,7 @@
 #a thing that grabs different timepoints of data, can set window and step size.
 import pandas as pd
 class Roller:
-    def __init__(self, file_path):
+    def __init__(self, file_path,gene_start=None,gene_end=None):
         """read the file, put it in a pandas dataframe? yeah that sounds fine"""
         self.raw_data = pd.read_csv(file_path,sep=" ")
         self.current_step = 0
@@ -10,9 +10,22 @@ class Roller:
         # get overall width of the time-course
         self.time_vec = self.raw_data['time'].unique()
         self.overall_width = len(self.time_vec)
+        if (gene_end != None):
+            self.gene_end = gene_end
+        else:
+            self.gene_end = len(self.raw_data.columns)
+        if (gene_start !=None):
+            self.gene_start = gene_start
+        else:
+            self.gene_start = 0
         self.current_window = self.get_window()
 
     def get_window(self):
+        raw_window = self.get_window_raw()
+        only_genes=raw_window.iloc[:,self.gene_start:self.gene_end]
+        return(only_genes)
+
+    def get_window_raw(self):
         start_index = self.current_step
         end_index = start_index + self.window_width
         time_window = self.time_vec[start_index:end_index]
--- test_roller.py
+++ test_roller.py
@@ -4,14 +4,20 @@ import numpy as np
 
 class TestRoller(unittest.TestCase):
     def setUp(self):
-        self.roller = Roller('compressed_katrina_data.txt')
+        self.roller = Roller('compressed_katrina_data.txt', 5)
 
     def test_next(self):
         self.roller.next()
-        window = self.roller.get_window()
+        window = self.roller.get_window_raw()
         time_slice = window['time'].unique()
         correct_window = [1,2,3]
         self.assertTrue(np.array_equal(correct_window, time_slice))
+
+    def test_get_only_genes(self):
+        only_genes = self.roller.get_window()
+        header = only_genes.columns.values
+        correct_header = ['AP1','AP2','AP3','AP4', 'AR', 'Bcat', 'Brachyury', 'cmyc', 'CRE', 'E2F','ELK1', 'ER', 'ETS1', 'FOXA', 'FOXO3A', 'GATA1', 'GATA2', 'GATA3','GLI', 'GR', 'HIF1', 'HNF1A', 'HOXA1', 'HSE', 'KLF4', 'LHX8','MEF2', 'MNX', 'MNX1', 'MYB', 'NANOG', 'NFAT', 'NFKb', 'NOBOX','Notch1', 'OCT', 'P53', 'PAX1', 'PEA3', 'PR', 'PTTG', 'RAR','RUNX1', 'RUNX2', 'SMAD1', 'SMAD3', 'SOX', 'SP1', 'SRF', 'STAT1','STAT3', 'STAT4', 'STAT5', 'VDR', 'WT1', 'YY1']
+        self.assertTrue(np.array_equal(correct_header, header))
 
 if __name__ == '__main__':
     unittest.main()
