7f35188dcbbe9a75a61ecc0a490e01a5be856d58
jfinkle <doubledaffy888@gmail.com>
2014-11-24 15:29:13 -0600

    Aupr modified, auroc added

--- aupr.py
+++ aupr.py
@@ -2,11 +2,8 @@ __author__ = 'jfinkle'
 
 import pandas as pd
 import numpy as np
-import sys
 import matplotlib.pyplot as plt
 from scipy import integrate
-
-# Load adjacency matrices
 
 def create_link_list(df, w):
     parent_names = df.index.values
@@ -18,13 +15,13 @@ def create_link_list(df, w):
     children = child_names[b.flatten()]
     directed_edges = df.values.flatten()
     all_edges = np.abs(directed_edges)
-    ll_array = np.array([parents, children, zip(parents, children), directed_edges, all_edges, weights])
+    ll_array = np.array([parents, children, zip(parents, children), directed_edges, all_edges, w])
     link_list = pd.DataFrame(ll_array).transpose()
     link_list.columns = ['Parent', 'Child', 'Edge', 'Directed_Edge', 'Edge_Exists', 'W']
     #link_list.sort(columns='Edge_Exists', ascending=False, inplace=True)
     return link_list
 
-def calc_roc(ref, pred):
+def calc_pr(ref, pred):
     # True Positive Rate (TPR) = TP/(TP+FN)
     # False Positive Rate (FPR) = FP/(FP+TN)
     ref.sort(columns='Edge', inplace=True)
@@ -75,6 +72,45 @@ def calc_roc(ref, pred):
 
     return precision, recall, aupr[-1]
 
+def calc_roc(ref, pred):
+    # True Positive Rate (TPR) = TP/(TP+FN)
+    # False Positive Rate (FPR) = FP/(FP+TN)
+    ref.sort(columns='Edge', inplace=True)
+    pred.sort(columns='Edge', inplace=True)
+    if not np.array_equal(ref.Edge.values, pred.Edge.values):
+        print 'Not same edges'
+        return
+
+    pred.sort(columns='W', ascending=False, inplace=True)
+    ref_edge_list = ref.Edge.values.tolist()
+    num_edges = len(ref_edge_list)
+
+    total_p = float(np.sum(ref.Edge_Exists.values))
+    total_n = len(ref.Edge_Exists.values) - total_p
+
+    tp = 0.0
+    fp = 0.0
+    tpr = []
+    fpr = []
+
+    for ii, row in enumerate(pred.iterrows()):
+        pred_edge = row[1].Edge
+        predicted = row[1].Edge_Exists
+        ref_idx = ref_edge_list.index(pred_edge)
+        real = ref.Edge_Exists.values[ref_idx]
+        if real and predicted:
+            tp+=1
+            #print real, predicted, 'tp'
+        elif not real and predicted:
+            fp +=1
+            #print real, predicted, 'fp'
+
+        tpr.append(tp/total_p)
+        fpr.append(fp/total_n)
+
+        auroc = integrate.cumtrapz(fpr, tpr)
+    return tpr, fpr, auroc[-1]
+
 xls = pd.ExcelFile('goldbetter_model/adjacency_matrix.xlsx')
 df = xls.parse()
 
@@ -85,11 +121,19 @@ np.random.seed(8)
 weights = np.random.random(len(df2)**2)
 reference = create_link_list(df, weights)
 random = np.array([np.sum(reference.Edge_Exists.values)/256.0]*256)
-prediction = create_link_list(df, weights)
-p, r, area = calc_roc(reference, prediction)
-plt.plot(r, p, 'o', r, random, 'r')
+prediction = create_link_list(df2, weights)
+p, r, area = calc_pr(reference, prediction)
+plt.plot(r, p, r, random, 'r')
 plt.xlabel('Recall')
 plt.ylabel('Precision')
 plt.legend(['Test', 'Random'])
+print area
+plt.show()
+
+tpr, fpr, area = calc_roc(reference, prediction)
+plt.plot(fpr, tpr, fpr, fpr, 'r')
+plt.xlabel('FPR')
+plt.ylabel('TPR')
+plt.legend(['Test', 'Random'], loc='best')
 print area
 plt.show()
