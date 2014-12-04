2c3f1c3e9d5208cb77af7c12c0e7195641952a57
jfinkle <doubledaffy888@gmail.com>
2014-12-04 14:23:27 -0600

    removed get_max_window_size from Roller. Redundant with property self.overall_width

--- Roller/unittests/test_roller_fit.py
+++ Roller/unittests/test_roller_fit.py
@@ -6,6 +6,7 @@ from sklearn.preprocessing import Imputer
 import matplotlib as mpl
 import numpy as np
 import pandas as pd
+import sys
 
 if __name__ == '__main__':
     file_path = "../../data/dream4/insilico_size10_1_timeseries.tsv"
@@ -15,9 +16,7 @@ if __name__ == '__main__':
     gene_end = None
 
     roll_me = Roller.Roller(file_path, gene_start_column, gene_end, time_label, separator)
-
-
-    window_size = 6
+    window_size = roll_me.overall_width
     #get only TFs data, window size of 4
     roll_me.set_window(window_size)
     #impute missing values
@@ -25,11 +24,8 @@ if __name__ == '__main__':
     mpl.rcParams.update({'font.size':8})
     mpl.rc('font',**{'family':'sans-serif','sans-serif':['Arial']})
     total_window_number = roll_me.get_n_windows()
-    #todo: fix the below two lines, I don't need this to get the matrix size...
     current_window = roll_me.get_window()
-    print current_window.shape
     filled_matrix = imputer.fit_transform(current_window)
     n_genes = filled_matrix.shape[1]
     coeff_matrix_3d = np.empty((n_genes,n_genes,total_window_number))
     gene_names=list(current_window.columns.values)
-    print filled_matrix.shape
