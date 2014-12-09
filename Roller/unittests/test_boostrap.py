c35eef94c2fa4b627255e715013394b17ca962e5
jfinkle <doubledaffy888@gmail.com>
2014-12-08 18:22:50 -0600

    bootstrap testing reveals that bootstrap code seems to function correctly, but with weird results

--- Roller/unittests/test_boostrap.py
+++ Roller/unittests/test_boostrap.py
@@ -7,6 +7,8 @@ import sys
 from Roller.util.linear_wrapper import LassoWrapper
 from Roller.util import Ranker
 from Roller.util import Grapher
+from scipy import stats
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     file_path = "../../data/dream4/insilico_size10_1_timeseries.tsv"
@@ -20,29 +22,49 @@ if __name__ == '__main__':
     boot = Ranker.Bootstrapper(roll_me)
     roll_me.set_window(roll_me.overall_width)
     window = roll_me.get_window()
-    lasso = LassoWrapper(window.values)
+    norm_values = stats.zscore(window.values.copy(), axis=1, ddof=1)
+    lasso = LassoWrapper(norm_values)
     max_alpha = lasso.get_max_alpha()
-    np.set_printoptions(suppress=True)
-    coefs = lasso.get_coeffs(max_alpha-1e-9)
 
-    boots = 10000
-    max_random = 1e-7
-    o = np.ones((5,5))
-    o = np.multiply(o, np.random.uniform(low=1-max_random, high=1+max_random, size=o.shape))
-    print o
-    exists = np.empty((boots))
-    for ii in range(boots):
-        n, p = window.values.shape
-        resample_values = window.values.copy()
-        # For each column randomly choose samples
-        #resample_values = np.array([np.random.choice(window.values[:,ii], size=n) for ii in range(p)]).T
-        noise = np.random.uniform(low=1-max_random, high=1+max_random, size=window.values.shape)
-        noisy_values = np.multiply(resample_values, noise)
-        lasso = LassoWrapper(noisy_values)
-        coefs = lasso.get_coeffs(max_alpha)
-        exists[ii] = coefs[0,4]
-    print np.count_nonzero(exists)/float(boots)
+    print max_alpha
 
+    boots = 100
+    max_random = 0.1
+    alphas = 10
+    #alpha_range = [max_alpha/10**aa for aa in range(alphas)]
+    alpha_range = np.linspace(0, max_alpha, 100)
+    print alpha_range
+    freqs = np.empty((len(alpha_range),4))
+    for kk, alpha in enumerate(alpha_range):
+        print kk
+        exists = np.empty((boots, 4))
+        for ii in range(boots):
+            n, p = norm_values.shape
+            # For each column randomly choose samples
+            resample_values = np.array([np.random.choice(norm_values[:, jj], size=n) for jj in range(p)]).T
+            noise = np.random.uniform(low=1-max_random, high=1+max_random, size=resample_values.shape)
+            noisy_values = np.multiply(norm_values, noise)
+            noisy_resample_values = np.multiply(resample_values, noise)
+
+            #Vanilla values
+            v_coefs = LassoWrapper(norm_values).get_coeffs(alpha)
+
+            # Noisy values
+            n_coefs = LassoWrapper(noisy_values).get_coeffs(alpha)
+
+            # Resample values
+            r_coefs = LassoWrapper(resample_values).get_coeffs(alpha)
+
+            # Noisy Resample
+            n_r_coefs = LassoWrapper(noisy_resample_values).get_coeffs(alpha)
+
+            exists[ii] = [v_coefs[0,4]!=0, n_coefs[0,4]!=0, r_coefs[0,4]!=0, n_r_coefs[0,4]!=0]
+        freqs[kk] = np.sum(exists, axis=0)/float(boots)
+
+    print freqs
+    plt.plot(alpha_range, freqs, 'o-')
+    plt.legend(['Reg', 'N', 'R', 'N+R'])
+    plt.show()
 
 
 
