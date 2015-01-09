c3a29948d2f6d1beade5d7c0760f60d7a88da3b8
jfinkle <doubledaffy888@gmail.com>
2015-01-09 16:34:49 -0600

    added resample_window to Window.py

--- Roller/Window.py
+++ Roller/Window.py
@@ -1,9 +1,29 @@
 __author__ = 'Justin Finkle'
 __email__ = 'jfinkle@u.northwestern.edu'
 
+import numpy as np
+import pandas as pd
+
+
 class Window(object):
     def __init__(self, dataframe, alpha=None):
-        self.dataframe = dataframe
+        self.df = dataframe
         self.alpha = alpha
-        self.data = dataframe.values
+        self.window_values = dataframe.values
 
+    def resample_window(self):
+        """
+        Resample window values, along a specific axis
+        :param window_values: array
+
+        :return: array
+        """
+        n, p = self.window_values.shape
+
+        # For each column randomly choose samples
+        resample_values = np.array([np.random.choice(self.window_values[:, ii], size=n) for ii in range(p)]).T
+
+        resample_window = pd.DataFrame(resample_values, columns=self.df.columns.values.copy(),
+                                       index=self.df.index.values.copy())
+
+        return resample_window
