507460f924b6132bdc748a9d2d4a5b52e4eac0c7
jfinkle <doubledaffy888@gmail.com>
2015-01-20 11:34:51 -0600

    Removed deprecated fuctions from roller

--- Roller/Roller.py
+++ Roller/Roller.py
@@ -95,95 +95,6 @@ class Roller(object):
     def get_n_genes(self):
         return(len(self.raw_data.columns) -1)
 
-    def fit_window(self, window, alpha):
-        """
-        Given a window get the lasso coefficients
-        :param window: data-frame
-            A roller window
-        :param alpha: float
-            Value to use for lasso regression
-        :return: array
-            Array of lasso beta regression coefficients
-
-        """
-        window_values = window.values
-        lasso = LassoWrapper(window_values)
-        beta_coef = lasso.get_coeffs(alpha)
-        return beta_coef
-
-    def fit_model(self, window_size, method='lasso', alpha=0.2, resamples=0, noise=0.2):
-        """
-        Fit the rolling model
-
-        :return: 3D matrix (n_windows, n_parents, n_children)
-            Matrix of coefficients. Each slice is the beta coefficient matrix for a given window
-        """
-
-        # Set the window size
-        self.set_window(window_size)
-
-        # Calculate total number of windows, and regressors, then initialize coefficient matrix
-        total_window_number = self.get_n_windows()
-        n_genes = len(self.gene_list)
-
-        if not resamples:
-            coeff_matrix_3d = np.empty((n_genes, n_genes, total_window_number))
-            for nth_window in range(total_window_number):
-                #loop gets the window, gets the coefficients for that window, then increments the window
-                current_window = self.get_window()
-                coeff_mat = self.fit_window(current_window, alpha)
-                coeff_matrix_3d[:, :, nth_window] = coeff_mat
-                #plot_figure(coeff_mat,nth_window,gene_names,gene_names,window_size)
-                self.next()
-
-            return coeff_matrix_3d
-
-        else:
-            coeff_matrix_4d = np.empty((n_genes, n_genes, total_window_number, resamples))
-            for nth_window in range(total_window_number):
-                #loop gets the window, gets the coefficients for that window, then increments the window
-                current_window = self.get_window()
-
-                for sample in range(resamples):
-                    sample_window = self.resample_window(current_window)
-                    noisy_window = self.add_noise_to_window(sample_window, noise)
-                    coeff_mat = self.fit_window(noisy_window, alpha)
-                    coeff_matrix_4d[:, :, nth_window, sample] = coeff_mat
-                self.next()
-
-            return coeff_matrix_4d
-
-    def resample_window(self, window):
-        """
-        Resample window values, along a specific axis
-        :param window_values: array
-
-        :return: array
-        """
-        window_values = window.values
-        n, p = window_values.shape
-
-        # For each column randomly choose samples
-        resample_values = np.array([np.random.choice(window_values[:, ii], size=n) for ii in range(p)]).T
-
-        resample_window = pd.DataFrame(resample_values, columns=window.columns.values.copy(),
-                                       index=window.index.values.copy())
-
-        return resample_window
-
-    def add_noise_to_window(self, window, max_random=0.2):
-        """
-        Add uniform noise to each value
-        :param window: dataframe
-
-        :param max_random: float
-            Amount of noise to add to each value, plus or minus
-        :return: array
-
-        """
-        noise = np.random.uniform(low=1-max_random, high=1+max_random, size=window.shape)
-        noisy_values = np.multiply(window, noise)
-        return noisy_values
 
     def zscore_all_data(self):
         #zscores all the data
@@ -228,11 +139,5 @@ class Roller(object):
                         'total_windows': self.get_n_windows(),
                         'window_index': window_index}
         return window_stats
-
-    def cross_validate_window(self, window, n_alphas=50, n_folds=3):
-        lasso = LassoWrapper(window.values)
-        alpha_range = np.linspace(0, lasso.get_max_alpha(), n_alphas)
-        q_squared_array = np.array([lasso.cross_validate_alpha(alpha, n_folds) for alpha in alpha_range])
-        return alpha_range, q_squared_array
 
 
