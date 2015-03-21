0361629fd5e889920ce2454b157239a14f8d4b1c
Jia Wu <jiawu@u.northwestern.edu>
2015-03-20 21:18:00 -0600

    added pipeline scripts to generate random windows

---
+++ cluster_h_random_pipeline.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#MSUB -A p20519
+#MSUB -l walltime=24:00:00
+#MSUB -l nodes=1:ppn=2
+#MSUB -M jiawu@u.northwestern.edu
+#MSUB -j oe
+#MSUB -o /projects/p20519/jia_output/Roller_error_h_random.txt
+#MSUB -m bae
+#MSUB -q normal
+#MSUB -N cluster_h
+#MSUB -V
+
+nwindows=${MOAB_JOBARRAYINDEX}
+
+workon seqgen
+module load python/anaconda
+cd ~/Roller
+python run_pipeline_h_random.py ${nwindows}
+
---
+++ run_pipeline_g_random.py
@@ -0,0 +1,50 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import sys
+import Roller
+import uuid
+import pickle
+if __name__ == "__main__":
+    window_size = int(sys.argv[1])
+    plt.ioff()
+    
+    for file_path_index in range(1,6):
+      file_path = "data/dream4/insilico_size10_" + str(file_path_index) + "_timeseries.tsv"
+      #file_path = "data/dream4/insilico_size10_1_timeseries.tsv"
+      gene_start_column = 1
+      time_label = "Time"
+      separator = "\t"
+      gene_end = None
+      gold_standard = "data/dream4/insilico_size10_" + str(file_path_index) + "_goldstandard.tsv"
+      #gold_standard = "data/dream4/insilico_size10_1_goldstandard.tsv"
+      image_file_path = "insilico_size10_" + str(file_path_index) + "_alphas"
+      #image_file_path = "insilico_size10_1_alphas"
+      roller = Roller.Roller(file_path, gene_start_column, gene_end, time_label,separator, window_type = "Lasso")
+      print("Overall Width: " + str(roller.overall_width))
+      roller.zscore_all_data()
+
+      #### My goal here is to test the whole range of alphas for the full window ####
+      alpha_list = []
+      aupr_list = []
+
+      roller.set_window(width=window_size)
+      roller.create_windows(random_time=True)
+      roller.optimize_params()
+      roller.fit_windows()
+      roller.rank_edges(n_bootstraps=200, permutation_n = 200)
+      roller.average_rank(rank_by='stability', ascending = False)
+      #score some edge lists
+      #first score the sorted average edge list
+      averaged_score_dict = roller.score(roller.averaged_ranks, gold_standard)
+      #next score each individual edge list
+      score_list = []
+      for window in roller.window_list:
+          score_dict = roller.score(window.results_table,gold_standard)
+          score_list.append(score_dict)
+      aupr_list.append(max(score_dict['aupr']))
+      
+      unique_filename = "/projects/p20519/Roller_outputs_lasso_random/"+ str(uuid.uuid4())
+      with open(unique_filename, 'wb') as output:
+        pickle.dump(roller,output, pickle.HIGHEST_PROTOCOL) 
+      
