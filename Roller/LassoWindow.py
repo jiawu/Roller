6ad61f45be763a4e9890fee5658d26413ffbff71
jfinkle <doubledaffy888@gmail.com>
2015-07-06 11:26:40 -0500

    LassoWindow reformatted to adhere to PEP8 standards

--- Roller/LassoWindow.py
+++ Roller/LassoWindow.py
@@ -59,63 +59,64 @@ class LassoWindow(Window):
 
     def generate_results_table(self):
 
-        #generate edges for initial model
+        # generate edges for initial model
         initial_edges = self.create_linked_list(self.beta_coefficients, 'B')
-        #permutation edges
-        permutation_mean_edges =self.create_linked_list(self.permutation_means, 'p-means')
+        # permutation edges
+        permutation_mean_edges = self.create_linked_list(self.permutation_means, 'p-means')
         permutation_sd_edges = self.create_linked_list(self.permutation_sd, 'p-sd')
         stability_edges = self.create_linked_list(self.edge_stability_auc, 'stability')
 
-        aggregated_edges = initial_edges.merge(permutation_mean_edges, on='regulator-target').merge(permutation_sd_edges, on='regulator-target').merge(stability_edges, on='regulator-target')
+        aggregated_edges = initial_edges.merge(permutation_mean_edges, on='regulator-target').merge(
+            permutation_sd_edges, on='regulator-target').merge(stability_edges, on='regulator-target')
 
-        #sorry, it is a little messy to do the p-value calculations for permutation tests here...
-        #valid_indices = aggregated_edges['p-sd'] != 0
-        #valid_indices = aggregated_edges['B'] != 0
+        # sorry, it is a little messy to do the p-value calculations for permutation tests here...
+        # valid_indices = aggregated_edges['p-sd'] != 0
+        # valid_indices = aggregated_edges['B'] != 0
         valid_window = aggregated_edges
         initial_B = valid_window['B']
         sd = valid_window['p-sd']
         mean = valid_window['p-means']
-        valid_window['final-z-scores-perm']=(initial_B - mean)/sd
-        valid_window['cdf-perm'] = (-1*abs(valid_window['final-z-scores-perm'])).apply(scipy.stats.norm.cdf)
-        #calculate t-tailed pvalue
-        valid_window['p-value-perm'] = (2*valid_window['cdf-perm'])
+        valid_window['final-z-scores-perm'] = (initial_B - mean) / sd
+        valid_window['cdf-perm'] = (-1 * abs(valid_window['final-z-scores-perm'])).apply(scipy.stats.norm.cdf)
+        # calculate t-tailed pvalue
+        valid_window['p-value-perm'] = (2 * valid_window['cdf-perm'])
         self.results_table = valid_window
-        return(self.results_table)
+        return self.results_table
 
     def rank_results(self, rank_by, ascending=False):
         rank_column_name = rank_by + "-rank"
         ##rank edges with an actual beta value first until further notice ##
         valid_indices = self.results_table['B'] != 0
         valid_window = self.results_table[valid_indices]
-        valid_window[rank_column_name] = valid_window[rank_by].rank(method="dense",ascending = ascending)
-        edge_n=len(valid_window.index)
+        valid_window[rank_column_name] = valid_window[rank_by].rank(method="dense", ascending=ascending)
+        edge_n = len(valid_window.index)
 
         invalid_indices = self.results_table['B'] == 0
         invalid_window = self.results_table[invalid_indices]
-        invalid_window[rank_column_name] = invalid_window[rank_by].rank(method="dense",ascending = ascending)
+        invalid_window[rank_column_name] = invalid_window[rank_by].rank(method="dense", ascending=ascending)
         invalid_window[rank_column_name] = invalid_window[rank_column_name] + edge_n
         self.results_table = valid_window.append(invalid_window)
-        self.results_table = self.results_table.sort(columns=rank_column_name, axis = 0)
+        self.results_table = self.results_table.sort(columns=rank_column_name, axis=0)
 
-        return(self.results_table)
+        return self.results_table
 
     def run_permutation_test(self, n_permutations=1000):
-        #initialize permutation results array
+        # initialize permutation results array
         self.permutation_means = np.empty((self.n_genes, self.n_genes))
         self.permutation_sd = np.empty((self.n_genes, self.n_genes))
         nth_window = self.nth_window
         zeros = np.zeros((self.n_genes, self.n_genes))
-        #initialize running calculation
-        result = {'n':zeros.copy(), 'mean':zeros.copy(), 'ss':zeros.copy()}
-        #inner loop: permute the window N number of times
+        # initialize running calculation
+        result = {'n': zeros.copy(), 'mean': zeros.copy(), 'ss': zeros.copy()}
+        # inner loop: permute the window N number of times
         for nth_perm in range(0, n_permutations):
-            #if (nth_perm % 200 == 0):
-                #print 'Perm Run: ' +str(nth_perm)
+            # if (nth_perm % 200 == 0):
+            # print 'Perm Run: ' +str(nth_perm)
 
-            #permute data
+            # permute data
             permuted_data = self.permute_data(self.window_values)
 
-            #fit the data and get coefficients
+            # fit the data and get coefficients
 
             permuted_coeffs = self.get_coeffs(self.alpha, permuted_data)
             dummy_list = []
@@ -134,45 +135,46 @@ class LassoWindow(Window):
         if sd is None:
             sd = self.permutation_sd.copy()
 
-        z_scores = (value - mean)/sd
-        cdf = stats.norm.cdf((-1*abs(z_scores)))
-        p_values = 2*cdf
+        z_scores = (value - mean) / sd
+        cdf = stats.norm.cdf((-1 * abs(z_scores)))
+        p_values = 2 * cdf
         return p_values
 
     def update_variance_2D(self, prev_result, new_samples):
         """incremental calculation of means: accepts new_samples, which is a list of samples. then calculates a new mean. this is a useful function for calculating the means of large arrays"""
-        n = prev_result["n"] #2D numpy array with all zeros or watev
-        mean = prev_result["mean"] #2D numpy array
-        sum_squares = prev_result["ss"] #2D numpy array
+        n = prev_result["n"]  # 2D numpy array with all zeros or watev
+        mean = prev_result["mean"]  # 2D numpy array
+        sum_squares = prev_result["ss"]  # 2D numpy array
 
-        #new_samples is a list of arrays
-        #x is a 2D array
+        # new_samples is a list of arrays
+        # x is a 2D array
         for x in new_samples:
             n = n + 1
-            #delta = float(x) - mean
+            # delta = float(x) - mean
             old_mean = mean.copy()
-            mean = old_mean + np.divide( (x-old_mean) , n)
-            sum_squares = sum_squares + np.multiply((x-mean),(x-old_mean))
+            mean = old_mean + np.divide((x - old_mean), n)
+            sum_squares = sum_squares + np.multiply((x - mean), (x - old_mean))
 
-        if (n[0,0] < 2):
-            result = {  "mean": mean,
-                        "ss": sum_squares,
-                        "n": n}
+        if (n[0, 0] < 2):
+            result = {"mean": mean,
+                      "ss": sum_squares,
+                      "n": n}
             return result
 
-        variance = np.divide(sum_squares,(n-1))
-        result = {  "mean": mean,
-                    "ss": sum_squares,
-                    "variance": variance,
-                    "n": n}
+        variance = np.divide(sum_squares, (n - 1))
+        result = {"mean": mean,
+                  "ss": sum_squares,
+                  "variance": variance,
+                  "n": n}
         return result
+
     def run_bootstrap(self, n_bootstraps=1000, n_alphas=20, noise=0.2):
         alpha_range = np.linspace(0, self.null_alpha, n_alphas)
         self.bootstrap_matrix = np.empty((self.n_genes, self.n_genes, n_bootstraps, n_alphas))
         for ii, alpha in enumerate(alpha_range):
             self.bootstrap_matrix[:, :, :, ii] = self.bootstrap_alpha(alpha, n_bootstraps, noise)
 
-        self.freq_matrix = self.calc_edge_freq()/float(n_bootstraps)
+        self.freq_matrix = self.calc_edge_freq() / float(n_bootstraps)
 
         self.edge_stability_auc = self.auc(self.freq_matrix, alpha_range)
 
@@ -212,7 +214,7 @@ class LassoWindow(Window):
         return edge_auc
 
     def get_nth_window_auc(self, nth):
-        auc = self.edge_stability_auc[:,:, nth]
+        auc = self.edge_stability_auc[:, :, nth]
         return auc
 
     def initialize_params(self, alpha=None):
@@ -263,7 +265,7 @@ class LassoWindow(Window):
         warnings.simplefilter("ignore")
         # Get maximum edges, assuming all explanors are also response variables and no self edges
         [n, p] = self.window_values.shape
-        max_edges = p * (p-1)
+        max_edges = p * (p - 1)
 
         # Raise exception if Lasso doesn't converge with alpha == 0
         if np.count_nonzero(self.get_coeffs(0)) != max_edges:
@@ -274,8 +276,8 @@ class LassoWindow(Window):
             raise ValueError('max_expected_alpha not high enough, coefficients still exist. Guess higher')
 
         # Set ranges of step sizes, assumed to be powers of 10
-        powers = int(np.log10(max_expected_alpha/min_step_size))
-        step_sizes = [max_expected_alpha/(10**ii) for ii in range(powers+1)]
+        powers = int(np.log10(max_expected_alpha / min_step_size))
+        step_sizes = [max_expected_alpha / (10 ** ii) for ii in range(powers + 1)]
 
         # Intialize loop values
         cur_min = 0
@@ -289,8 +291,8 @@ class LassoWindow(Window):
                 cur_max = alpha_max
 
             # Set the current step size and new range to look through
-            cur_step = step_sizes[ii+1]
-            cur_range = np.linspace(cur_min, cur_max, (cur_max-cur_min)/cur_step+1)
+            cur_step = step_sizes[ii + 1]
+            cur_range = np.linspace(cur_min, cur_max, (cur_max - cur_min) / cur_step + 1)
 
             # In the current range, check when coefficients start popping up
             for cur_alpha in cur_range:
@@ -325,7 +327,7 @@ class LassoWindow(Window):
 
         # Calculate the cv_table values
         cv_results = np.array([self.cross_validate_alpha(alpha, n_folds, True) for alpha in alpha_range])
-        column_labels = np.append(self.genes+"_Q^2", ["Model_Q^2", "positive_q2"])
+        column_labels = np.append(self.genes + "_Q^2", ["Model_Q^2", "positive_q2"])
 
         # Put the results into a dataframe
         df = pd.DataFrame(cv_results, columns=column_labels)
@@ -338,11 +340,11 @@ class LassoWindow(Window):
             cv_selected_alpha = alpha_range[df["Model_Q^2"].idxmax(1)]
 
         elif method == 'max_posQ2':
-            #todo: an secondary criteria needs to be employed if this is used because there will likely be multiple alphas with the same number of positive Q2
+            # todo: an secondary criteria needs to be employed if this is used because there will likely be multiple alphas with the same number of positive Q2
             cv_selected_alpha = alpha_range[df["positive_q2"].idxmax(1)]
 
         else:
-            raise ValueError("User entered method %s is not valid" %method)
+            raise ValueError("User entered method %s is not valid" % method)
 
         return cv_selected_alpha, df.copy()
 
@@ -372,17 +374,17 @@ class LassoWindow(Window):
             y_predicted = np.dot(x_test, current_coef)
 
             # Calculate PRESS and SS
-            current_press = np.sum(np.power(y_predicted-y_test, 2), axis=0)
+            current_press = np.sum(np.power(y_predicted - y_test, 2), axis=0)
             current_ss = self.sum_of_squares(y_test)
 
             press += current_press
             ss += current_ss
 
         # Calculate the Q^2 value of each gene
-        q_squared = 1-press/ss
+        q_squared = 1 - press / ss
 
         # Calculate the Q^2 of the whole model. This is different than averaging the individual q_squared values
-        model_q_squared = 1 - np.sum(press)/np.sum(ss)
+        model_q_squared = 1 - np.sum(press) / np.sum(ss)
 
         if condensed:
             return np.append(q_squared, [model_q_squared, np.sum(q_squared > 0)])
@@ -398,7 +400,7 @@ class LassoWindow(Window):
             The sum of squares, columnwise or total
         """
         column_mean = np.mean(X, axis=0)
-        ss = np.sum(np.power(X-column_mean, 2), axis=0)
+        ss = np.sum(np.power(X - column_mean, 2), axis=0)
         return ss
 
     def get_coeffs(self, alpha, data=None):
@@ -410,37 +412,37 @@ class LassoWindow(Window):
             Data to fit. If none, will use the window values. Default is None
         :return:
         """
-        #loop that iterates through the target genes
+        # loop that iterates through the target genes
         if data is None:
             all_data = self.window_values.copy()
         else:
             all_data = data.copy()
 
-        coeff_matrix = np.array([],dtype=np.float_).reshape(0, all_data.shape[1])
+        coeff_matrix = np.array([], dtype=np.float_).reshape(0, all_data.shape[1])
 
         model_list = []
 
-        for col_index,column in enumerate(all_data.T):
-            #Instantiate a new Lasso object
+        for col_index, column in enumerate(all_data.T):
+            # Instantiate a new Lasso object
             clf = linear_model.Lasso(alpha)
 
-            #delete the column that is currently being tested
+            # delete the column that is currently being tested
             X_matrix = np.delete(all_data, col_index, axis=1)
-            #take out the column so that the gene does not regress on itself
-            target_TF = all_data[:,col_index]
+            # take out the column so that the gene does not regress on itself
+            target_TF = all_data[:, col_index]
             clf.fit(X_matrix, target_TF)
-            model_params = {'col_index':col_index,
-                            'response':target_TF,
-                            'predictor':X_matrix,
-                            'model':clf}
+            model_params = {'col_index': col_index,
+                            'response': target_TF,
+                            'predictor': X_matrix,
+                            'model': clf}
             model_list.append(model_params)
             coeffs = clf.coef_
-            #artificially add a 0 to where the col_index is
-            #to prevent self-edges
-            coeffs = np.insert(coeffs,col_index,0)
-            coeff_matrix=np.vstack((coeff_matrix,coeffs))
+            # artificially add a 0 to where the col_index is
+            # to prevent self-edges
+            coeffs = np.insert(coeffs, col_index, 0)
+            coeff_matrix = np.vstack((coeff_matrix, coeffs))
 
-            #scoping issues
+            # scoping issues
             training_scores, test_scores = self.crag_window(model_params)
             self.training_scores.append(training_scores)
             self.test_scores.append(test_scores)
