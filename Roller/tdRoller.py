d2875f6bb977c186bb0c86f5365cc41a6213ff8f
jfinkle <doubledaffy888@gmail.com>
2015-07-15 14:13:06 -0500

    Changed tdRoller so method make_sort_df() can sort based on different ways

--- Roller/tdRoller.py
+++ Roller/tdRoller.py
@@ -26,6 +26,7 @@ class tdRoller(Roller):
                  window_type)
         self.full_edge_list = None
         self.edge_dict = None
+        self.lag_set = None
 
         # Zscore the data
         self.norm_data = self.zscore_all_data()
@@ -144,15 +145,18 @@ class tdRoller(Roller):
             df = df[df.Parent != df.Child]
 
         df['Edge'] = zip(df.Parent, df.Child)
+        df['Lag'] = df.C_window - df.P_window
         self.full_edge_list = df.copy()
+        self.lag_set = list(set(self.full_edge_list.Lag))
         print "[DONE]"
         return
 
-    def make_static_edge_dict(self, self_edges=False):
+    def make_static_edge_dict(self, true_edges, self_edges=False):
         """
         Make a dictionary of edges
         :return:
         """
+        print "Lumping edges...",
         df = self.full_edge_list.copy()
         if not self_edges:
             df = df[df.Parent != df.Child]
@@ -161,22 +165,33 @@ class tdRoller(Roller):
 
         for edge in edge_set:
             current_df = df[df.Edge==edge]
-            self.edge_dict[edge] = {"dataframe":current_df, "mean_importance":np.mean(current_df.Importance)}
+            max_idx = current_df['Importance'].idxmax()
+            lag_imp = np.mean([np.median(current_df.Importance[current_df.Lag==lag]) for lag in self.lag_set])
+            self.edge_dict[edge] = {"dataframe":current_df, "mean_importance":np.median(current_df.Importance),
+                                    'real_edge':(edge in true_edges), "max_importance":current_df.Importance[max_idx],
+                                    'max_edge':(current_df.P_window[max_idx], current_df.C_window[max_idx]),
+                                    'lag_importance': lag_imp}
 
+        print "[DONE]"
         return
 
-    def calc_edge_mean(self, df):
+    def make_sort_df(self, df, sort_by='mean'):
         """
         Calculate the mean for each edge
         :param df: dataframe
         :return: dataframe
         """
-        temp_dict = {edge:df[edge]['mean_importance'] for edge in df.keys()}
-        mean_df = pd.DataFrame.from_dict(temp_dict, orient='index')
-        mean_df.columns = ['mean_importance']
-        mean_df.sort('mean_importance', ascending=False, inplace=True)
-        mean_df['regulator-target'] = mean_df.index
-        return mean_df
+        sort_field = sort_by+"_importance"
+        print "Calculating %s edge importance..." %sort_by,
+        temp_dict = {edge:df[edge][sort_field] for edge in df.keys()}
+        sort_df = pd.DataFrame.from_dict(temp_dict, orient='index')
+        sort_df.columns = [sort_field]
+        sort_df.sort(sort_field, ascending=False, inplace=True)
+        #sort_df['mean_importance'] = stats.zscore(sort_df['mean_importance'], ddof=1)
+        sort_df.index.name = 'regulator-target'
+        sort_df=sort_df.reset_index()
+        print "[DONE]"
+        return sort_df
 
     def calc_edge_importance_cutoff(self, df):
         """
@@ -198,45 +213,109 @@ class tdRoller(Roller):
         :param gold_standard_file:
         :return:
         """
+        print "Scoring model...",
         if gold_standard_file is None:
             current_gold_standard = self.file_path.replace("timeseries.tsv","goldstandard.tsv")
         else:
             current_gold_standard = gold_standard_file
         evaluator = Evaluator(current_gold_standard, '\t')
         tpr, fpr, auroc = evaluator.calc_roc(sorted_edge_list)
-        auroc_dict = {'tpr':tpr, 'fpr':fpr, 'auroc': auroc}
+        auroc_dict = {'tpr':np.array(tpr), 'fpr':np.array(fpr), 'auroc': np.array(auroc)}
         precision, recall, aupr = evaluator.calc_pr(sorted_edge_list)
         aupr_random = [len(evaluator.gs_flat)/float(len(evaluator.full_list))]*len(recall)
-        aupr_dict = {"precision": precision, "recall": recall, "aupr": aupr, "aupr_random":aupr_random}
+        aupr_dict = {"precision": np.array(precision), "recall": np.array(recall), "aupr": np.array(aupr),
+                     "aupr_random": np.array(aupr_random)}
+        print "[DONE]"
         return auroc_dict, aupr_dict
 
+    def plot_scoring(self, roc_dict, pr_dict):
+        f, axarr = plt.subplots(2, figsize=(7,10))
+        axarr[0].plot(roc_dict['fpr'], roc_dict['tpr'])
+        axarr[0].plot(roc_dict['fpr'], roc_dict['fpr'])
+        axarr[0].legend(['tdRoller', 'Random'], loc='best')
+        axarr[0].set_xlabel('False Positive Rate')
+        axarr[0].set_ylabel('True Positive Rate')
+        title = 'ROC Curve (AUROC = %0.3f)' %roc_dict['auroc'][-1]
+        axarr[0].set_title(title)
+        axarr[1].plot(pr_dict['recall'], pr_dict['precision'])
+        axarr[1].plot(pr_dict['recall'], pr_dict['aupr_random'])
+        axarr[1].legend(['tdRoller', 'Random'], loc='best')
+        axarr[1].set_xlabel('Recall')
+        axarr[1].set_ylabel('Precision')
+        title = 'PR Curve (AUPR = %0.3f)' %pr_dict['aupr'][-1]
+        axarr[1].set_title(title)
+        plt.tight_layout()
+        plt.show()
+        return
+
 if __name__ == "__main__":
-    file_path = "../data/dream4/insilico_size10_2_timeseries.tsv"
+    file_path = "../data/dream4/insilico_size100_4_timeseries.tsv"
     gene_start_column = 1
     time_label = "Time"
     separator = "\t"
     gene_end = None
+    current_gold_standard = file_path.replace("timeseries.tsv","goldstandard.tsv")
+    evaluator = Evaluator(current_gold_standard, '\t')
+    true_edges = evaluator.gs_flat.tolist()
+    print true_edges
     #pd.set_option('display.width', 1000)
 
-    #np.random.seed(8)
+    np.random.seed(8)
 
     tdr = tdRoller(file_path, gene_start_column, gene_end, time_label, separator)
     tdr.zscore_all_data()
-    tdr.set_window(13)
+    tdr.set_window(9)
     tdr.create_windows()
     tdr.augment_windows()
     tdr.fit_windows(n_trees=10)
     tdr.compile_roller_edges(self_edges=True)
-    tdr.make_static_edge_dict()
-    df2 = tdr.calc_edge_mean(tdr.edge_dict)
+    #tdr.full_edge_list = tdr.full_edge_list[tdr.full_edge_list.Lag>1]
+    tdr.make_static_edge_dict(true_edges)
+    df2 = tdr.make_sort_df(tdr.edge_dict, 'lag')
     roc_dict, pr_dict = tdr.score(df2)
-    f, axarr = plt.subplots(2)
-    axarr[0].plot(roc_dict['fpr'], roc_dict['tpr'])
-    axarr[0].plot(roc_dict['fpr'], roc_dict['fpr'])
-    axarr[1].plot(pr_dict['recall'], pr_dict['precision'])
-    axarr[1].plot(pr_dict['recall'], pr_dict['aupr_random'])
-    plt.show()
-
-
+    print roc_dict['auroc'][-1]+(1-roc_dict['fpr'][-1])
+    print pr_dict['aupr'][-1]+(1-pr_dict['recall'][-1])
+    tdr.plot_scoring(roc_dict, pr_dict)
     sys.exit()
+
+    f, axarr = plt.subplots(4, 4)
+    ii=0
+    jj=0
+    lag_set = list(set(tdr.full_edge_list.Lag))
+    lag_set.sort()
+    for edge, current_dict in tdr.edge_dict.iteritems():
+        current_df = current_dict['dataframe']
+        rank = df2.index[df2['regulator-target']==edge].values
+        if rank<=15:
+            if current_dict['real_edge']:
+                p_color = 'green'
+            else:
+                p_color = 'red'
+            lag_imp = [np.average(current_df.Importance[current_df.Lag==lag]) for lag in lag_set]
+            axarr[ii, jj].scatter(lag_set, lag_imp, color=p_color)
+            axarr[ii, jj].set_title(str(edge)+str(current_dict['real_edge'])+str(rank))
+            jj+=1
+            if jj==4:
+                jj=0
+                ii+=1
+    plt.tight_layout()
+    plt.show()
+    sys.exit()
+    roc_dict, pr_dict = tdr.score(df2)
+
+    f, axarr = plt.subplots(9, 10)
+    for ii, key in enumerate(tdr.edge_dict.keys()):
+        violin = axarr[ii/10, ii%10].violinplot(tdr.edge_dict[key]['dataframe']['Importance'].values)
+        axarr[ii/10, ii%10].set_title(key)
+        if key in true_edges:
+            v_color = 'green'
+        else:
+            v_color = 'red'
+        for pc in violin['bodies']:
+            pc.set_facecolor(v_color)
+    plt.show()
+    #plt.plot(2*pr_dict['precision']*pr_dict['recall']/(pr_dict['precision']+pr_dict['recall']))
+    #plt.tight_layout()
+    #plt.show()
+    #tdr.plot_scoring(roc_dict, pr_dict)
 
