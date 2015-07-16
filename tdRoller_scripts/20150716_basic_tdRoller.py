a56d70da9aaea440a7f2f0e52724e60a5a2bb917
jfinkle <doubledaffy888@gmail.com>
2015-07-16 09:51:25 -0500

    Added basic script for running tdRoller

---
+++ tdRoller_scripts/20150716_basic_tdRoller.py
@@ -0,0 +1,34 @@
+__author__ = 'Justin Finkle'
+__email__ = 'jfinkle@u.northwestern.edu'
+
+from Roller.tdRoller import tdRoller
+from Roller.util.Evaluator import Evaluator
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+insilico_n = 1
+file_path = "../data/dream4/insilico_size10_%i_timeseries.tsv"%insilico_n
+gene_start_column = 1
+time_label = "Time"
+separator = "\t"
+gene_end = None
+current_gold_standard = file_path.replace("timeseries.tsv","goldstandard.tsv")
+evaluator = Evaluator(current_gold_standard, '\t')
+true_edges = evaluator.gs_flat.tolist()
+
+np.random.seed(8)
+
+tdr = tdRoller(file_path, gene_start_column, gene_end, time_label, separator)
+tdr.zscore_all_data()
+tdr.set_window(10)
+tdr.create_windows()
+tdr.augment_windows()
+tdr.fit_windows(n_trees=10, show_progress=False)
+tdr.compile_roller_edges(self_edges=True)
+#tdr.full_edge_list = tdr.full_edge_list[tdr.full_edge_list.Lag>1]
+tdr.make_static_edge_dict(true_edges)
+df2 = tdr.make_sort_df(tdr.edge_dict, 'lag')
+roc_dict, pr_dict = tdr.score(df2)
+print roc_dict['auroc'][-1]+(1-roc_dict['fpr'][-1])
+print pr_dict['aupr'][-1]+(1-pr_dict['recall'][-1])
