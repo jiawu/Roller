c7304e137f736de15b6ac4bd000ecdea93a96268
jfinkle <doubledaffy888@gmail.com>
2015-11-05 16:23:55 -0600

    Add additional criteria for lags to meet. Add methods to set min and max lag

--- tdRoller_scripts/20150716_basic_tdRoller.py
+++ tdRoller_scripts/20150716_basic_tdRoller.py
@@ -9,9 +9,9 @@ import pandas as pd
 import sys
 
 insilico_n = 4
-window_width = 18
-min_lag = 4
-max_lag = 5
+window_width = 17
+min_lag = 0
+max_lag = 1
 n_trees = 10
 n_permutes = 10
 mse_adjust = False
