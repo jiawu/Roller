5fc943a8d53a934110ce51e7bde3b0c28d5ce882
Jia Wu <jiawu@u.northwestern.edu>
2015-11-13 15:20:00 -0600

    analyze_box_plots.py changed to analyze comparative results between methods
    Former-commit-id: d25145a62d81a6d1bf23e3a04139cce6c078d931

--- scripts/analyze_box_plots.py
+++ scripts/analyze_box_plots.py
@@ -1,6 +1,6 @@
 import matplotlib
 matplotlib.use('Agg')
-from Swing_old.util.BoxPlot import BoxPlot
+from Swing.util.BoxPlot import BoxPlot
 import pdb
 
 import numpy as np
@@ -15,15 +15,17 @@ Script that loads data from a dataframe and generates boxplots
 """
 
 output_path = "/home/jjw036/"
-input_folder = "/projects/p20519/roller_output/stability_analysis/RandomForest/"
+
+input_folder_list = ["/projects/p20519/roller_output/stability_analysis/RandomForest/","/projects/p20519/roller_output/stability_analysis/Lasso/","/projects/p20519/roller_output/stability_analysis/Dionesus/"]  
 test_statistic = 'aupr'
-save_tag = "Insilico_aupr_n_perms_stability"
-n_trials = 200
+save_tag = "Insilico_methods_comparison_aupr"
+n_trials = 50
 
 agg_df = pd.DataFrame()
-for file_path in os.listdir(input_folder):    
-    df = pd.read_csv(input_folder+file_path,sep=',|\t', engine='python')
-    agg_df = agg_df.append(df)
+for input_folder in input_folder_list:
+    for file_path in os.listdir(input_folder):    
+        df = pd.read_csv(input_folder+file_path,sep=',|\t', engine='python')
+        agg_df = agg_df.append(df)
 
 datasets = ['insilico_size10_1','insilico_size10_2','insilico_size10_3','insilico_size10_4','insilico_size10_5']
 
@@ -31,22 +33,43 @@ label_list = []
 auroc_list = []
 
 #stability analysis
-for dataset in datasets:
+for dataset in datasets[0:1]:
     current_df = agg_df[agg_df['file_path'].str.contains(dataset)]
-    test_stat_10 = current_df[(current_df['n_trees'] ==500) & (current_df['permutation_n']==10)]
-    test_stat_100 = current_df[(current_df['n_trees'] ==500) & (current_df['permutation_n']==100)]
-    test_stat_500 = current_df[(current_df['n_trees'] ==500) & (current_df['permutation_n']==500)]
-    test_stat_1000 = current_df[(current_df['n_trees'] ==500) & (current_df['permutation_n']==1000)]
+    test_stat_D_21 = current_df[(current_df['data_folder'].str.contains('Dionesus')) & (current_df['td_window'] == 21)]
+    test_stat_L_21 = current_df[(current_df['data_folder'].str.contains('Lasso')) & (current_df['td_window'] == 21)]
+    test_stat_RF_21 = current_df[(current_df['data_folder'].str.contains('RandomForest')) & (current_df['td_window'] == 21)]
     pdb.set_trace()
-    auroc_list.append(test_stat_10[test_statistic][0:n_trials].tolist())
-    auroc_list.append(test_stat_100[test_statistic][0:n_trials].tolist())
-    auroc_list.append(test_stat_500[test_statistic][0:n_trials].tolist())
-    auroc_list.append(test_stat_1000[test_statistic][0:n_trials].tolist())
     
-    label_list.append("n_perm = 10")
-    label_list.append("n_perm = 100")
-    label_list.append("n_perm = 500")
-    label_list.append("n_perm = 1000")
+    test_stat_D_10 = current_df[(current_df['data_folder'].str.contains('Dionesus')) & (current_df['td_window'] == 10)]
+    test_stat_L_10 = current_df[(current_df['data_folder'].str.contains('Lasso')) & (current_df['td_window'] == 10)]
+    test_stat_RF_10 = current_df[(current_df['data_folder'].str.contains('RandomForest')) & (current_df['td_window'] == 10)]
+
+    test_stat_D_15 = current_df[(current_df['data_folder'].str.contains('Dionesus')) & (current_df['td_window'] == 15)]
+    test_stat_L_15 = current_df[(current_df['data_folder'].str.contains('Lasso')) & (current_df['td_window'] == 15)]
+    test_stat_RF_15 = current_df[(current_df['data_folder'].str.contains('RandomForest')) & (current_df['td_window'] == 15)]
+
+
+    auroc_list.append(test_stat_D_21[test_statistic][0:n_trials].tolist())
+    auroc_list.append(test_stat_L_21[test_statistic][0:n_trials].tolist())
+    auroc_list.append(test_stat_RF_21[test_statistic][0:n_trials].tolist())
+    
+    auroc_list.append(test_stat_D_10[test_statistic][0:n_trials].tolist())
+    auroc_list.append(test_stat_L_10[test_statistic][0:n_trials].tolist())
+    auroc_list.append(test_stat_RF_10[test_statistic][0:n_trials].tolist())
+    
+    auroc_list.append(test_stat_D_15[test_statistic][0:n_trials].tolist())
+    auroc_list.append(test_stat_L_15[test_statistic][0:n_trials].tolist())
+    auroc_list.append(test_stat_RF_15[test_statistic][0:n_trials].tolist())
+    
+    label_list.append("Dionesus")
+    label_list.append("Lasso")
+    label_list.append("RandomForest")
+    label_list.append("td-Dionesus, w=10")
+    label_list.append("td-Lasso, w=10")
+    label_list.append("td-RandomForest, w=10")
+    label_list.append("td-Dionesus, w=15")
+    label_list.append("td-Lasso, w=15")
+    label_list.append("td-RandomForest, w=15")
 
 
 bp_data = auroc_list
