b9dafeea43c85c4d62c9e9fcf76e5d8d7374d873
Jia Wu <jiawu@u.northwestern.edu>
2017-01-30 12:29:56 -0600

    cleaned up directory

--- scripts/average_results.py
+++
@@ -1,9 +0,0 @@
-
-# get a list of DFs by reading the csv, append
-
-# average list of DFs based off of 'regulator-target'
-
-big_df = pd.concat(parsed_df_list)
-mean_df=big_df.groupby(level=0).mean()
-
-
