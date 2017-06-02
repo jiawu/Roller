c36808f2550e0842d915618fe246ae4ad9c528cf
jfinkle <doubledaffy888@gmail.com>
2017-06-02 17:34:07 -0500

    Make preliminary figures for AUROC/AUPR change in 100node networks

--- scripts/100node_comparison/box_plot_large_network_comparison.py
+++ scripts/100node_comparison/box_plot_large_network_comparison.py
@@ -1,8 +1,8 @@
 import matplotlib
+import seaborn as sns
 
-matplotlib.use('Agg')
-from Swing.util.BoxPlot import BoxPlot
-from matplotlib.backends.backend_pdf import PdfPages
+# from Swing.util.BoxPlot import BoxPlot
+# from matplotlib.backends.backend_pdf import PdfPages
 
 import pdb
 
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import sys
 import os
+from scipy.stats import mannwhitneyu
 
 pd.set_option('display.width', 2000)
 """
@@ -28,8 +29,8 @@ def read_tdr_results(folder_list):
                 df = pd.read_csv(input_folder + file_path, sep=',|\t', engine='python')
 
             # Correct weird inputs:
-            # Omranian data includes a weird "alpha" at the beginning which shifts and messes up data.
-            if 'alpha' in df.columns:
+            # Omranian data has a messed up header
+            if 'omranian' in file_path:
                 new_cols = df.columns[1:]
                 # Delete last column
                 del df[df.columns[-1]]
@@ -68,8 +69,9 @@ def parse_tdr_results(agg_df, test_statistic, datasets):
 
 
 output_path = "./"
+methods = ['Dionesus', 'Lasso', 'RandomForest']
 
-input_folder_list = ["/Users/jfinkle/Downloads/Lasso/"]
+input_folder_list = ["/Users/jfinkle/Downloads/RandomForest/"]
 test_statistic = ['aupr', 'auroc']
 save_tag = "Dionesus_Yeast100_11-20"
 n_trials = 100
@@ -77,6 +79,44 @@ n_trials = 100
 datasets = ["Yeast100-" + str(index) + "_" for index in range(1, 21)]
 # datasets = ['insilico_size10_1','insilico_size10_2','insilico_size10_3','insilico_size10_4','insilico_size10_5']
 agg_df = read_tdr_results(input_folder_list)
+g = agg_df.groupby('file_path')
+net_stats = []
+for name, group in g:
+    # Quick and dirty parsing
+    if 'Yeast' in name or 'Ecoli' in name:
+        net_name = os.path.basename(name).split('_')[0]
+    else:
+        net_name = os.path.basename(name).split('_t')[0]
+    system = net_name.split('100')[0].split('_')[0]
+    subgroup = group.groupby('td_window')
+    sg_mean = subgroup.mean()
+    sg_std = subgroup.std()
+    try:
+        current_stats = [net_name, system, sg_mean.loc[21, 'aupr'], sg_std.loc[21, 'aupr'],
+                         sg_mean.loc[15, 'aupr'], sg_std.loc[15, 'aupr'],
+                         sg_mean.loc[21, 'auroc'], sg_std.loc[21, 'auroc'],
+                         sg_mean.loc[15, 'auroc'], sg_std.loc[15, 'auroc']]
+        net_stats.append(current_stats)
+    except:
+        pass
+
+net_stats = pd.DataFrame(net_stats, columns=['network', 'system', 'base_aupr_mean', 'base_aupr_std', 'swing_aupr_mean',
+                                             'swing_aupr_std', 'base_auroc_mean', 'base_auroc_std', 'swing_auroc_mean',
+                                             'swing_auroc_std'])
+net_stats['aupr_diff'] = (net_stats['swing_aupr_mean']-net_stats['base_aupr_mean'])/net_stats['base_aupr_mean']*100
+net_stats['auroc_diff'] = (net_stats['swing_auroc_mean']-net_stats['base_auroc_mean'])/net_stats['base_auroc_mean']*100
+
+sns.boxplot(data=net_stats, x='system', y='auroc_diff')
+plt.ylabel('AUROC Increase (%)')
+plt.savefig('./RF_100_node_auroc_increase.pdf', format='pdf')
+plt.close()
+
+sns.boxplot(data=net_stats, x='system', y='aupr_diff')
+plt.ylabel('AUPR Increase (%)')
+plt.savefig('./RF_100_node_aupr_increase.pdf', format='pdf')
+for n, g in net_stats.groupby('system'):
+    print(n, mannwhitneyu(g.base_aupr_mean, g.swing_aupr_mean), mannwhitneyu(g.base_auroc_mean, g.swing_auroc_mean))
+sys.exit()
 
 with PdfPages(output_path + save_tag + '.pdf') as pdf:
     for test in test_statistic:
