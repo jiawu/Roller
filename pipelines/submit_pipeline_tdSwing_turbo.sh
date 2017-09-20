d47b72412249e6a111f68996e5b107f1c535ddee
Jia Wu <jiawu@u.northwestern.edu>
2017-09-20 06:12:49 -0500

    removed trailing spaces in here-document

--- pipelines/submit_pipeline_tdSwing_turbo.sh
+++ pipelines/submit_pipeline_tdSwing_turbo.sh
@@ -1,8 +1,7 @@
 #!/bin/bash     
 while read P1 P2 P3 P4 P5
 do
-    JOB=`msub - << EOJ        
-
+    JOB=`msub - << EOJ
 #MSUB -A p20519
 #MSUB -l walltime=24:00:00
 #MSUB -l nodes=1:ppn=1
@@ -17,11 +16,11 @@ do
 workon seqgen
 module load python/anaconda3
 cd /home/jjw036/Roller/pipelines
-                                                                                                                                   
+
 echo "python run_tdSwing_scan.py ${P1} ${P2} ${P3} ${P4} ${P5}"
-python run_tdSwing_scan.py ${P1} ${P2} ${P3} ${P4} ${P5}                                                                                                                                   
-EOJ  
+python run_tdSwing_scan.py ${P1} ${P2} ${P3} ${P4} ${P5}
+EOJ
 `
 echo "JobID = ${JOB} for parameters ${P1} ${P2} ${P3} ${P4} ${P5} submitted on `date`"
-done < job_params_large_networks.txt
+done < job_params_1.txt
 exit
