38c18d8a26644c0c79a3d31cf99e031e3cf42fc3
Jia Jie Wu <jia@dhcp-10-105-126-175.wireless.northwestern.private>
2017-09-27 13:44:30 -0500

    i'm committing more frequently because i am editing your files.

--- scripts/figure3/gardner_scan_results.py
+++ scripts/figure3/gardner_scan_results.py
@@ -1,10 +1,48 @@
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt
 from scipy import stats
-import numpy as np
-import sys
+
 from palettable.colorbrewer.qualitative import Set1_3
+
+plt.rcParams['font.size'] = 24
+plt.rcParams['axes.labelsize'] = 24
+plt.rcParams['xtick.labelsize'] = 24
+plt.rcParams['ytick.labelsize'] = 24
+
+plt.rcParams['savefig.dpi'] = 300
+plt.rcParams['savefig.bbox'] = 'tight'
+plt.rcParams['savefig.pad_inches'] = 0.1
+
+plt.rcParams['figure.facecolor'] = "white"
+plt.rcParams['text.color'] = "black"
+plt.rcParams['axes.labelcolor'] = "black"
+plt.rcParams['legend.frameon'] = "False"
+plt.rcParams['legend.numpoints'] = 1
+plt.rcParams['legend.scatterpoints'] = 1
+plt.rcParams['legend.fontsize'] = 18
+plt.rcParams['legend.markerscale'] = 1
+plt.rcParams['xtick.direction'] = 'in'
+plt.rcParams['ytick.direction'] = 'in'
+plt.rcParams['xtick.color'] = 'black'
+plt.rcParams['ytick.color'] = 'black'
+plt.rcParams['axes.axisbelow'] = True
+plt.rcParams['image.cmap'] = 'Greys'
+plt.rcParams['font.family'] = 'sans-serif'
+plt.rcParams['font.sans-serif'] = ["Arial", "sans-serif"]
+
+plt.rcParams['axes.grid'] = False
+plt.rcParams['axes.facecolor'] = "white"
+plt.rcParams['axes.edgecolor'] = "black"
+plt.rcParams['axes.linewidth'] = 1.25
+plt.rcParams['xtick.major.size'] = 5
+plt.rcParams['xtick.minor.size'] = 5
+plt.rcParams['ytick.major.size'] = 0
+plt.rcParams['ytick.minor.size'] = 0
+
 
 df = pd.read_pickle('gardner_RF_scan_1000trees.pkl')
 df['minmax'] = list(zip(df.min_lag, df.max_lag))
@@ -34,6 +72,7 @@ for w in range(2, 14):
 colors.append('k')
 
 sns.boxplot(data=pdata, x='td_window', y='aupr', palette=colors)
+
 # plt.plot([-1, 12], [baseline_mean, baseline_mean], '-k', zorder=0)
 plt.show()
 
